96c942ac480d4179a02a97ce6cb453eef8c92764
3llimist <jas.c.owen@gmail.com>
2026-08-21 20:53:35 +1000

    wasm

--- RooForms.service.worker.js
+++ RooForms.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787307274|924713546';
+const CACHE_VERSION = '1787309588|3238382198';
 /** @type {string} */
 const CACHE_PREFIX = 'RooForms-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
